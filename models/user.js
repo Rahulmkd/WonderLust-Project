27c9a17334b9aa9d8ba3e7dc0ea33781ee9fa8ca
Rahul Mahakud <rulmkd@gmail.com>
2026-01-23 02:09:07 +0530

    Fix passport-local-mongoose import and resolve signup error

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose").default;
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
   },
 });
 
